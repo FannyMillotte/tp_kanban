3efe43f36ed083ea8f2522bc74ff542058da2c20
Adam Creusevault <adam.creusevault@etu.univ-nantes.fr>
2023-12-06 15:04:14 +0100

    I made the drag and drop with boxes

--- drag_and_drop.py
+++ drag_and_drop.py
@@ -7,12 +7,12 @@ pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 450
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen : pygame.Surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Drag And Drop')
 
 
-items = []
-
+active_box = None
+items : list[pygame.Rect] = []
 for i in range(5):
     x = random.randint(50, 700)
     y = random.randint(50, 350)
@@ -20,15 +20,41 @@ for i in range(5):
     w = random.randint(35, 65)
     h = random.randint(35, 65)
 
+    box = pygame.Rect(x, y, w, h)
+    items.append(box)
 
 
 
 run = True
 while run :
     screen.fill('turquoise1')
-    pygame.display.flip()
+
+    for box in items:
+        pygame.draw.rect(screen, "purple", box)
+
     for event in pygame.event.get():
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                for num, box in enumerate(items):
+                    if box.collidepoint(event.pos):
+                        active_box = num
+                    pass
+                pass
+            pass
+        
+        if event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                active_box = None
+
+
+        if event.type == pygame.MOUSEMOTION:
+            if active_box != None:
+                items[active_box].move_ip(event.rel)
+            pass
+
         if event.type == pygame.QUIT:
             run = False
 
+    pygame.display.flip()
     pass
