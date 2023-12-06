3ba24eae725884611f729812ff039f03f1c4dbde
Fanny <millotte.fanny@outlook.fr>
2023-12-06 15:07:39 +0100

    Ajout de la date de fin, de la suppression de tache et de l'assignation.

--- main.py
+++ main.py
@@ -3,8 +3,11 @@
 Created on Wed Dec  6 10:00:52 2023
 
 """
+import locale
 from datetime import datetime
 from datetime import timedelta
+
+locale.setlocale(locale.LC_TIME, 'fr-FR.utf8')
 
 class Tache: 
     def __init__(self, index, description, duree, status):
@@ -12,16 +15,17 @@ class Tache:
         self.description = description
         self.attribue_a = None
         self.duree = duree #jours
-        self.date_creation = datetime.now().strftime("%m/%d/%Y")
+        self.date_creation = datetime.now().strftime("%d/%m/%Y")
+        self.date_fin = self.get_date_fin()
         self.status = status
         
     def __str__(self):
-        return f"Tache {self.index}: {self.description} créée le {self.date_creation} terminée le " 
+        return f"Tache {self.index}: {self.description} créée le {self.date_creation} terminée le {self.date_fin} attribué à {self.attribue_a}" 
     
     def get_date_fin(self):
-        date = datetime.strptime(self.date_creation, "%m/%d/%Y")
+        date = datetime.strptime(self.date_creation, "%d/%m/%Y")
         date_fin = date + timedelta(days=self.duree)
-        return date_fin.strftime("%m/%d/%Y")
+        return date_fin.strftime("%d/%m/%Y")
 
 
 class Kanban:
@@ -38,7 +42,6 @@ class Kanban:
         
         for i in range(0, len(self.taches)):
             for j in range(0, len(self.taches[i])):
-                #if self.taches[i][j] != None:
                 txt += str(self.taches[i][j]) + "  "
             txt += "\n"
         return txt
@@ -47,15 +50,34 @@ class Kanban:
         self.taches.append([Tache(self.nombre_taches + 1, 
                                   description_tache, 2, 
                                   self.noms_colonnes[0])])
+        self.nombre_taches += 1
         
+    def supprimer_tache(self, index_tache):
+        for i in range(0, len(self.taches)):
+            for j in range(0, len(self.taches[i])):
+                if self.taches[i][j].index == index_tache:
+                    self.taches[i].remove(self.taches[i][j])
+                    return True
+        return False
+    
+    def attribuer_tache(self, index_tache, utilisateur):
+        for i in range(0, len(self.taches)):
+            for j in range(0, len(self.taches[i])):
+                if self.taches[i][j].index == index_tache:
+                    self.taches[i][j].attribue_a = utilisateur
         
 
 colonnes = ["To Do", "In Progress", "Done"]
 kanban = Kanban(colonnes)
-            
-print(kanban)
 
 kanban.ajout_tache("Nouvelle tache")
 kanban.ajout_tache("Nouvelle tache 2")
 print(kanban)
+
+kanban.supprimer_tache(2)
+print(kanban)
+
+kanban.attribuer_tache(1, "Toto")
+print(kanban)
+
             
